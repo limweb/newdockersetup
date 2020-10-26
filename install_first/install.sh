c771b2bb2278c50862f439c283a56ef733f8c125
lim web <limweb@hotmail.com>
2020-10-26 07:15:21 +0000

    addinstallscript

---
+++ install_first/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+username=$(whoami)
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt  install docker.io
+sudo systemctl status docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+sudo docker network create nginx-network
+sudo usermod -a -G docker $username
+#//---------------- end -------------------
+#sudo docker-compose  up -d //example
+#sudo docker-compose -f ./mariadb.yml up -d   //example
